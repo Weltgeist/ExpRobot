83ab03b38823b6412c1cd27fe9990d4fdc3a462d
Samuel Narcisse <narcisse118@gmail.com>
2021-02-17 18:53:38 -0500

    added motor, controller and drive

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,9 +4,28 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
+  // Declare all of our variables
+
+  // Motors
+  WPI_TalonFX leftFront;
+  WPI_TalonFX rightFront;
+  WPI_TalonFX leftRear;
+  WPI_TalonFX rightRear;
+
+  // Controllers
+  SpeedControllerGroup rightSide;
+  SpeedControllerGroup leftSide;
+
+  // Drives
+  DifferentialDrive drive;
+  
   /** Creates a new DriveTrain. */
   public DriveTrain() {}
 
